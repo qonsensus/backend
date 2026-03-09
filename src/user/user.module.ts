3896a231c509a50a58de0b571864d4108d37570a
skostadinov0141 <simeon.kostadinov0141@gmail.com>
2026-03-09 22:30:52 +0100

    refactor: remove friendship-related endpoints and logic
    Changes:
    - Remove friendship request, accept, and reject endpoints from UserController.
    - Remove friendship-related methods from UserService.
    - Update UserModule to exclude Friendship entity.
    
    Generated by GitHub Copilot

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -4,11 +4,10 @@ import { Profile } from '../entities/profile.entity';
 import { User } from '../entities/user.entity';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
-import { Friendship } from '../entities/friendship.entity';
 import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, Profile, Friendship]), AuthModule],
+  imports: [TypeOrmModule.forFeature([User, Profile]), AuthModule],
   providers: [UserService],
   controllers: [UserController],
 })
